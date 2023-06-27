fdf442ba075b0d7685f624e0e0432689612226f0
Dp_ras <51804858+DSQL-MONGKEY@users.noreply.github.com>
2023-06-27 23:17:15 +0700

    Changing about paragraph

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,19 +15,19 @@ export const About = () => {
             </h2>
          </motion.div>
 
-         <motion.p variants={fadeIn("", "", 0.5, 1)}
-            className={`mt-4 text-zinc-800 dark:text-slate-300 font-poppins px-5 text-xl sm:w-1/2 `}>
-            <span className=" mt-3 sm:mt-10">
+         <motion.div variants={fadeIn("", "", 0.5, 1)}
+            className={`mt-4 text-zinc-800 flex flex-col gap-2 dark:text-slate-300 font-poppins px-5 text-xl sm:w-1/2 `}>
+            <p className=" mt-3 sm:mt-10">
                Terdiri dari 19 orang mahasiswa/i yang memiliki semangat dan cita-cita yang tinggi dalam meraih masa depan yang lebih baik, Mendapat kesempatan menempuh dalam nya sumur ilmu pengetahuan.
-            </span> <br /> <br />
-            <span className="sm:my-16">
+            </p> &nbsp;
+            <p className="">
                <span className="font-semibold text-3xl">Teknik Komputer</span>
                , mampu mendesain dan mengimplementasi sistem yang terintegrasi baik software maupun hardware.
-            </span>  
-            <span>
-               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem ea aliquid nesciunt id magnam? Quasi aspernatur praesentium vel omnis dolorum, at assumenda pariatur delectus est aut, illum eaque repellat odit?
-            </span>
-         </motion.p>
+            </p> &nbsp; 
+            <p>
+               <span className="font-semibold text-sm capitalize">*intinya pengen lulus tepat waktu</span>
+            </p>
+         </motion.div>
       </section>
    )
 }
